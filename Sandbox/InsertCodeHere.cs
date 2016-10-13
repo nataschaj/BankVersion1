96175d5c04463c06f633b6dc633b889ffd0ba51a
Natascha Jakobsen <natascha_jakobsen@hotmail.com>
2016-10-13 13:24:28 +0200

    Added new object and Console.Writeline

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -7,6 +7,10 @@ namespace Sandbox
         public void MyCode()
         {
             // The FIRST line of code should be BELOW this line
+            BankAccount account1 = new BankAccount("Natascha");
+
+            Console.WriteLine($"{account1.GetName()} has a balance of {account1.GetBalance()} ");
+
 
 
             // The LAST line of code should be ABOVE this line
