9aa8ce2f80ef226a1f87ba27398088f169c19908
Natascha Jakobsen <natascha_jakobsen@hotmail.com>
2016-10-13 13:41:51 +0200

    Withdraw values added

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -9,13 +9,16 @@ namespace Sandbox
             // The FIRST line of code should be BELOW this line
             BankAccount account1 = new BankAccount("Natascha");
 
-            Console.WriteLine($"{account1.GetName()} has a balance of {account1.GetBalance()} ");
+            Console.WriteLine($"{account1.GetName()} has a bank account balance of {account1.GetBalance()} ");
 
             account1.Deposit(200);
             account1.Deposit(300);
 
-            Console.WriteLine($"{account1.GetName()} has a balance of {account1.GetBalance()} ");
+            Console.WriteLine($"{account1.GetName()} has a bank account balance of {account1.GetBalance()} ");
 
+            account1.Withdraw(100);
+
+            Console.WriteLine($"{account1.GetName()} has a bank account balance of {account1.GetBalance()} ");
             // The LAST line of code should be ABOVE this line
         }
     }
